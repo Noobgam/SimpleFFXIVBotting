c44949f71ccc4cb62aa0e4d1de224e4bb4850b5a
Noobgam <shessmaster12@gmail.com>
2026-03-25 07:16:20 +0100

    chore: add innocence

--- msqhelper.lua
+++ msqhelper.lua
@@ -38,6 +38,7 @@ local questStepIdToDungeonId = {
     [2553] = { [3] = 239 }, -- the royal menagerie
     [3074] = { [3] = 537 }, -- castrum fluminis
     [3320] = { [2] = 657 }, -- titania (the dancing plague)
+    [3643] = { [5] = 666 }, -- innocence (the crown of the immaculate)
 }
 
 local primalDungeons = { 59, 60, 61 }
