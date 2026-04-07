c991c136a4552701ee3ff86aa4854464bd2d5432
Noobgam <shessmaster12@gmail.com>
2026-04-07 19:59:54 +0200

    chore: add emanation as proper profile

--- kdfProfiles.lua
+++ kdfProfiles.lua
@@ -141,6 +141,55 @@ if NoobgamKdfProfiles == nil then
             },
             excludeavoid = {},
         },
+        [719] = {
+            name = "Emanation",
+            mesh = "[Trial] Emanation",
+            dutyid = 719,
+            level = 67,
+            expansion = 4,
+            creator = "Koyote/Rinn",
+            notes = "",
+            queuetype = 2,
+            FFA = false,
+            hacks = false,
+            requeuetimer = 10,
+            objectivedestinations = {
+                [1] = {objective = 1, pos = {x = 100, y = 0, z = 100}},
+            },
+            interacts = {},
+            bossids = {
+                6385, -- Lakshmi -- Lakshmi Card
+            },
+            forcemeleerange = {6386},
+            enemytargetdistance = 50,
+            prioritytarget = {},
+            tankat = {
+                [1] = {contentid = 6386, frompercent = 100, topercent = 1, pos = {x = -4, y = 0, z = -3}, desc = "Tank at this pos from 100-1%"},
+            },
+            advancedavoid = {
+                [1] = {type = "custom", customdetails = "function", functionname = "customfunction", functioncode = [[
+                        function customfunction()
+                            local a = MEntityList("contentid=6385")
+                            if a ~= nil then
+                                for b, c in pairs(a) do
+                                    if c.action ~= nil then
+                                        if c.action == 7748 then
+                                            if ActionList:Get(5, 26):IsReady() then
+                                                Player:Stop()
+                                                ActionList:Get(5, 26):Cast()
+                                            end
+                                        end
+                                    end
+                                end
+                            end
+                        end
+                    ]]
+                },--to phase 2
+            },
+            hasbuff = {},
+            overheadmarkers = {},
+            excludeavoid = {},
+        },
         [778] = {
             name = "Castrum Fluminis",
             mesh = "",
