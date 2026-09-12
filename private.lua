59f6056c8f62de3a14e67daf7fc2992fc61376b4
Noobgam <shessmaster12@gmail.com>
2026-09-12 15:31:47 +0200

    chore: fix some profiles being stuck

--- private.lua
+++ private.lua
@@ -1,14 +1,47 @@
 ---@diagnostic disable: duplicate-set-field
 NoobgamPrivateAPI = {}
 
+local function log(msg)
+    d("[NoobgamPrivateAPI] " .. msg)
+end
+
 function NoobgamPrivateAPI.SetKDFToNone()
     if KitanoiSettings == nil or kIO == nil then
         return
     end
 
+    KitanoiFuncs.EnableAddon("kdf", false)
+
     if KitanoiSettings.KDFIntegration ~= 1 then
         KitanoiSettings.SingleOrQueue = 1
         KitanoiSettings.KDFIntegration = 1
+        kIO.save()
+    end
+
+    local dungeonName = KDF.Name()
+    if dungeonName ~= nil and dungeonName:lower() ~= "none" then
+        log("Clearing selected KDF dungeon: " .. dungeonName)
+        KitanoiFuncs.LoadDungeonTbl({
+            name = "None",
+            dutyid = 9999999,
+            hacks = false,
+            enemylos = true,
+            queuetype = 1,
+            interactdistance = 5,
+            enemytargetdistance = 5,
+            prioritytargetdistance = 5,
+            objectivedestinations = {},
+            hasbuff = {},
+            interacts = {},
+            dontclearfriendlytargets = {},
+            useaction = {},
+            tankat = {},
+            bossids = {},
+            prioritytarget = {},
+            forcemeleerange = {},
+            advancedavoid = {},
+            overheadmarkers = {},
+        })
         kIO.save()
     end
 end
